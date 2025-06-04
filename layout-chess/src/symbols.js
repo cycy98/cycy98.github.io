81bee7b1edac567828230d2f0ebea6cfbca2d71c
cycy98 <152851979+cycy98@users.noreply.github.com>
2025-06-04 14:39:16 +0200

    Update symbols.js

--- layout-chess/src/symbols.js
+++ layout-chess/src/symbols.js
@@ -5,24 +5,27 @@
  */
 
 const symbols = {
-  // diacritics, represented by a space + a combining character
-  '*`': ' \u0300', // grave
-  '*´': ' \u0301', // acute
-  '*^': ' \u0302', // circumflex
-  '*~': ' \u0303', // tilde
-  '*¯': ' \u0304', // macron
-  '*˘': ' \u0306', // breve
-  '*˙': ' \u0307', // dot above
-  '*¨': ' \u0308', // diaeresis
-  '*˚': ' \u030a', // ring above
-  '*”': ' \u030b', // double acute
-  '*ˇ': ' \u030c', // caron
-  '*‟': ' \u030f', // double grave
-  '*⁻': ' \u0311', // inverted breve
-  '*.': ' \u0323', // dot below
-  '*,': ' \u0326', // comma below
-  '*¸': ' \u0327', // cedilla
-  '*˛': ' \u0328', // ogonek
+  // diacritics, represented by a dotted circle + a combining character
+  '*`': '◌\u0300', // grave
+  '*´': '◌\u0301', // acute
+  '*^': '◌\u0302', // circumflex
+  '*~': '◌\u0303', // tilde
+  '*¯': '◌\u0304', // macron
+  '*˘': '◌\u0306', // breve
+  '*˙': '◌\u0307', // dot above
+  '*¨': '◌\u0308', // diaeresis
+  '*˚': '◌\u030a', // ring above
+  '*”': '◌\u030b', // double acute
+  '*ˇ': '◌\u030c', // caron
+  '*‟': '◌\u030f', // double grave
+  '*⁻': '◌\u0311', // inverted breve
+  '*.': '◌\u0323', // dot below
+  '*,': '◌\u0326', // comma below
+  '*¸': '◌\u0327', // cedilla
+  '*˛': '◌\u0328', // ogonek
+  '*b': '𝕓', // balckboard bold
+  "*I": "𝐼", // italic
+  '*g': '𝐠'
   // special keys, represented by a smaller single character
   // '*/': stroke   (no special glyph needed)
   // '*µ': greek    (no special glyph needed)
